af7c8d908d3adbfeacb30d50a3fc0c0a9c4011c6
eldemet <eldemet@gmail.com>
2016-08-31 19:01:57 +0300

    f_y function

---
+++ book-examples/Chapter1/system-model-overview/f_y.m
@@ -0,0 +1,32 @@
+function [y] = f_y(input1)
+%FUNCTION_NAME - One line description of what the function or script performs (H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%
+% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+%
+% Inputs:
+%    input1 - Description
+%
+% Outputs:
+%    output1 - Description
+%
+% Example: 
+%    Line 1 of example
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: none
+
+% Author        : Demetrios G. Eliades
+% Work address  : KIOS Research Center, University of Cyprus
+% email         : eldemet@ucy.ac.cy
+% Website       : http://www.kios.ucy.ac.cy
+% Last revision : September 2016
+
+%------------- BEGIN CODE --------------
+
+output1=input1;
+
+%------------- END OF CODE --------------

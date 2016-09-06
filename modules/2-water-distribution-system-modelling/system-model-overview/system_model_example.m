331e2dae7fbb1a5caf6c714e7e203746507941e9
eldemet <eldemet@gmail.com>
2016-09-06 16:47:33 +0300

    changing the parameters

--- modules/2-water-distribution-system-modelling/system-model-overview/system_model_example.m
+++ modules/2-water-distribution-system-modelling/system-model-overview/system_model_example.m
@@ -1,17 +1,17 @@
-filename = which('Net1_Rossman2000.inp')
+filename = 'Net1_Rossman2000.inp';
 
-Sigma = epanet(filename);
+G = epanet(filename);
 
-hydraulic_states = Sigma.getComputedHydraulicTimeSeries;
-quality_states = Sigma.getComputedQualityTimeSeries;
+hydraulic_states = G.getComputedHydraulicTimeSeries;
+quality_states = G.getComputedQualityTimeSeries;
 
-tank_index = Sigma.NodeTankIndex;
-node_index = Sigma.NodeJunctionIndex;
-link_index = Sigma.LinkPipeIndex;
+tank_index = G.NodeTankIndex;
+node_index = G.NodeJunctionIndex;
+link_index = G.LinkPipeIndex;
 
 % tank levels/volume
 x.Tank_Volume = [hydraulic_states.Time hydraulic_states.TankVolume(:,tank_index)];
-x.Tank_Chlorine = [quality_states.Time quality_states.Quality(:, tank_index)]
+x.Tank_Chlorine = [quality_states.Time quality_states.Quality(:, tank_index)];
 
 z.Junctions_Head = [hydraulic_states.Time hydraulic_states.Head(:, node_index)];
 z.Pipe_Flow = [hydraulic_states.Time hydraulic_states.Flow(:, link_index)];
@@ -19,9 +19,13 @@ c.Junctions_Chlorine = [quality_states.Time quality_states.Quality(:, node_index
 
 e_y = 0;
 
-y = f_y(x,z,c) + e_y;
+% Specify sensor array 's'
+% 1) Tank Volume; 2) Tank Chlorine; Junctions Head 1,3,9; Junctions
+% Chlorine 5,7
+s = {[1], [1], [1 3 9], [], [5,7]}; 
 
+% Construct output vector y
+y = f_y(x,z,c,G,s) + e_y;
 
-
-Sigma.unload
+G.unload
 

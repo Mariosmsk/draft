c0b62f7d2968f1510adc4cd218a0e38231021e7d
eldemet <eldemet@gmail.com>
2016-09-06 12:08:25 +0300

    Update system model

--- book-examples/Chapter1/system-model-overview/system_model_example.m
+++ book-examples/Chapter1/system-model-overview/system_model_example.m
@@ -1,13 +1,13 @@
 filename = which('Net1_Rossman2000.inp')
 
-S = epanet(filename);
+Sigma = epanet(filename);
 
-hydraulic_states = S.getComputedHydraulicTimeSeries;
-quality_states = S.getComputedQualityTimeSeries;
+hydraulic_states = Sigma.getComputedHydraulicTimeSeries;
+quality_states = Sigma.getComputedQualityTimeSeries;
 
-tank_index = G.NodeTankIndex;
-node_index = G.NodeJunctionIndex;
-link_index = G.LinkPipeIndex;
+tank_index = Sigma.NodeTankIndex;
+node_index = Sigma.NodeJunctionIndex;
+link_index = Sigma.LinkPipeIndex;
 
 % tank levels/volume
 x.Tank_Volume = [hydraulic_states.Time hydraulic_states.TankVolume(:,tank_index)];
@@ -17,5 +17,11 @@ z.Junctions_Head = [hydraulic_states.Time hydraulic_states.Head(:, node_index)];
 z.Pipe_Flow = [hydraulic_states.Time hydraulic_states.Flow(:, link_index)];
 c.Junctions_Chlorine = [quality_states.Time quality_states.Quality(:, node_index)];
 
-S.unload
+e_y = 0;
+
+y = f_y(x,z,c) + e_y;
+
+
+
+Sigma.unload
 

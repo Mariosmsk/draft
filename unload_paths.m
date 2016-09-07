4e6dd72092725fdfb50a073a4b234ec0daf62fa3
Marios Kyriakou <mariosmsk@gmail.com>
2016-09-07 12:22:30 +0300

    unload paths function

---
+++ unload_paths.m
@@ -0,0 +1,33 @@
+function [output] = load_paths()
+%LOAD_PATHS Loads all the folder paths in the memory. 
+%Run this function before executing the book examples.
+%
+% Syntax:  load_paths()
+%
+% Inputs:
+%    none
+%
+% Outputs:
+%    output - String of paths in Matlab memory.
+%
+% Example: 
+%    load_paths()
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: none
+
+% Author        : Demetrios G. Eliades
+% Work address  : KIOS Research Center, University of Cyprus
+% email         : eldemet@ucy.ac.cy
+% Website       : http://www.kios.ucy.ac.cy
+% Last revision : September 2016
+
+%------------- BEGIN CODE --------------
+rmpath(genpath(pwd))
+output = 'Paths unloaded.';    
+%------------- END OF CODE --------------
+
+

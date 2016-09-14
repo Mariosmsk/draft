d1ae10f54dcf50cfc2379c8bfeaf62d3e7ad79ab
Marios Kyriakou <mariosmsk@gmail.com>
2016-09-14 15:32:59 +0300

    update

--- modules/5-quality-simulations/2-simulate-quality-msx/two_source_chlorine_decay.m
+++ modules/5-quality-simulations/2-simulate-quality-msx/two_source_chlorine_decay.m
@@ -13,6 +13,25 @@ results = output;
 % %% Extract output from JSON
 % tmp = loadjson(output);
 % results=tmp.results;
+u=ones(1,4); clear pp1 pp2 pp3 pp4
+for i=1:length(results.nodeID)
+    ppNodesvalue(i) = results.compQualNodes.Quality{i}{1}(end);
+    if ppNodesvalue(i)<0.2
+        pp1{u(1)}=results.linkID{i}; u(1)=u(1)+1;
+    end
+    if ppNodesvalue(i)>0.2 && ppNodesvalue(i)<0.4
+        pp2{u(2)}=results.linkID{i}; u(2)=u(2)+1;
+    end
+    if ppNodesvalue(i)>0.4 && ppNodesvalue(i)<0.6
+        pp3{u(3)}=results.linkID{i}; u(3)=u(3)+1;
+    end
+    if ppNodesvalue(i)>0.6
+        pp4{u(4)}=results.linkID{i}; u(4)=u(4)+1;
+    end
+end
+d = epanet(settings.filename);
+d.plot('highlightnode',pp1);
+
 for u=1:length(results.nodeID)
     figure;cmap=hsv(5);
     for i=1:results.MSXSpeciesCount
@@ -24,30 +43,18 @@ for u=1:length(results.nodeID)
     ylabel('Quantity');
     xlabel('Time(sec)');
 end
-%     
-% %% Plot graph
-% for u=1:length(results.nodeID)
-%     figure(settings.figure);subplot(2,2,u);cmap=hsv(5);
-%     for i=1:results.MSXSpeciesCount
-%         plot(results.compQualNodes.Time,results.compQualNodes.Quality{settings.index(u)}{i},'Color',cmap(i,:));
-%         hold on; 
-%     end
-%     legend(results.MSXSpeciesNameID);
-%     title(['NODE ',char(results.nodeID(u))]);
-%     ylabel('Quantity');
-%     xlabel('Time(sec)');
-% end
-% 
-% for u=3:4
-%     figure(settings.figure);subplot(2,2,u);cmap=hsv(5);
-%     for i=1:results.MSXSpeciesCount
-%         plot(results.compQualLinks.Time,results.compQualLinks.Quality{settings.index(u-2)}{i},'Color',cmap(i,:));
-%         hold on; 
-%     end
-%     legend(results.MSXSpeciesNameID)
-%     title(['LINK ',char(results.linkID(u-2))]);
-%     ylabel('Quantity');
-%     xlabel('Time(sec)');
-% end
-% 
+
+for u=1:length(results.linkID)
+    figure;cmap=hsv(5);
+    for i=1:results.MSXSpeciesCount
+        plot(results.compQualLinks.Time,results.compQualLinks.Quality{settings.index(u)}{i},'Color',cmap(i,:));
+        hold on; 
+    end
+    legend(results.MSXSpeciesNameID);
+    title(['LINK ',char(results.linkID(u))]);
+    ylabel('Quantity');
+    xlabel('Time(sec)');
+end
+
+
 %------------- END OF CODE --------------

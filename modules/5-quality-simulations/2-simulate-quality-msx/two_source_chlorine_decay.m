b9adb56176e77ab876fce41ed31eb6c1777aa043
Marios Kyriakou <mariosmsk@gmail.com>
2016-09-15 18:11:55 +0300

    update

--- modules/5-quality-simulations/2-simulate-quality-msx/two_source_chlorine_decay.m
+++ modules/5-quality-simulations/2-simulate-quality-msx/two_source_chlorine_decay.m
@@ -3,7 +3,7 @@ settings.filename = 'Net3_MSXexample.inp'; % net2-cl2
 settings.msxfilename = 'Net3_two_source_chlorine_decay.msx'; % net2-cl2
 % settings.index = [1:97]; %node or link index
 settings.figure = 1;
-settings.duration = 700;
+settings.duration = 24;
 %% Create JSON file for input
 input = savejson(settings);
 
@@ -11,23 +11,25 @@ input = savejson(settings);
 output = simulate_msx_two_source(input);
 results = output;
 
+% load results.mat
 % nodes
 u=ones(1,5); pp1n=[]; pp2n=[]; pp3n=[]; pp4n=[]; pp5n=[];
+ppNodesvalue=[];
 for i=1:length(results.nodeID)
     ppNodesvalue(i) = mean(results.compQualNodes.Quality{i}{1});
     if ppNodesvalue(i)<0.2
         pp1n{u(1)}=results.nodeID{i}; u(1)=u(1)+1;
     end
-    if ppNodesvalue(i)>0.2 && ppNodesvalue(i)<0.4
+    if ppNodesvalue(i)>=0.2 && ppNodesvalue(i)<0.4
         pp2n{u(2)}=results.nodeID{i}; u(2)=u(2)+1;
     end
-    if ppNodesvalue(i)>0.4 && ppNodesvalue(i)<0.6
+    if ppNodesvalue(i)>=0.4 && ppNodesvalue(i)<0.6
         pp3n{u(3)}=results.nodeID{i}; u(3)=u(3)+1;
     end
-    if ppNodesvalue(i)>0.6 && ppNodesvalue(i)<0.8
+    if ppNodesvalue(i)>=0.6 && ppNodesvalue(i)<0.8
         pp4n{u(4)}=results.nodeID{i}; u(4)=u(4)+1;
     end
-    if ppNodesvalue(i)>0.8
+    if ppNodesvalue(i)>=0.8
         pp5n{u(5)}=results.nodeID{i}; u(5)=u(5)+1;
     end    
 end
@@ -39,21 +41,22 @@ c5n = cell(1,length(pp5n)); c5n(:) = {'r'};
  
 % links
 u=ones(1,5); pp1=[]; pp2=[]; pp3=[]; pp4=[]; pp5=[];
+ppLinksvalue=[];
 for i=1:length(results.linkID)
     ppLinksvalue(i) = mean(results.compQualLinks.Quality{i}{1});
     if ppLinksvalue(i)<0.2
         pp1{u(1)}=results.linkID{i}; u(1)=u(1)+1;
     end
-    if ppLinksvalue(i)>0.2 && ppLinksvalue(i)<0.4
+    if ppLinksvalue(i)>=0.2 && ppLinksvalue(i)<0.4
         pp2{u(2)}=results.linkID{i}; u(2)=u(2)+1;
     end
-    if ppLinksvalue(i)>0.4 && ppLinksvalue(i)<0.6
+    if ppLinksvalue(i)>=0.4 && ppLinksvalue(i)<0.6
         pp3{u(3)}=results.linkID{i}; u(3)=u(3)+1;
     end
-    if ppLinksvalue(i)>0.6 && ppLinksvalue(i)<0.8
+    if ppLinksvalue(i)>=0.6 && ppLinksvalue(i)<0.8
         pp4{u(4)}=results.linkID{i}; u(4)=u(4)+1;
     end
-    if ppLinksvalue(i)>0.8
+    if ppLinksvalue(i)>=0.8
         pp5{u(5)}=results.linkID{i}; u(5)=u(5)+1;
     end    
 end
@@ -67,6 +70,8 @@ h=results.d.plot('highlightlink',[pp1 pp2 pp3 pp4 pp5],'colorlink',[c1 c2 c3 c4 
     'highlightnode',[pp1n pp2n pp3n pp4n pp5n],'colornode',[c1n c2n c3n c4n c5n],'nodes','yes');
 h2=results.d.plot('highlightlink',[pp1 pp2 pp3 pp4 pp5],'colorlink',[c1 c2 c3 c4 c5],...
     'highlightnode',[pp1n pp2n pp3n pp4n pp5n],'colornode',[c1n c2n c3n c4n c5n]);
+h2=results.d.plot('highlightlink',[pp1 pp2 pp3 pp4 pp5],'colorlink',[c1 c2 c3 c4 c5],...
+    'highlightnode',[pp1n pp2n pp3n pp4n pp5n],'colornode',[c1n c2n c3n c4n c5n],'links','yes');
 
 results.d.unloadMSX
 results.d.unload

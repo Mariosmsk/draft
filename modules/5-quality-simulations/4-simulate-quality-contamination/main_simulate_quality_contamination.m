335e1831ca1eeebaca0ab2e8f4f79f41840855ef
Marios Kyriakou <mariosmsk@gmail.com>
2016-10-07 14:31:35 +0300

    update simulate - contamination

--- modules/5-quality-simulations/4-simulate-quality-contamination/main_simulate_quality_contamination.m
+++ modules/5-quality-simulations/4-simulate-quality-contamination/main_simulate_quality_contamination.m
@@ -1,27 +1,47 @@
 settings=[];
-settings.filename = which('Net1_Rossman2000.inp');
+settings.filename = 'Net1.inp';
 
-%% Create JSON file for input
-input = savejson(settings);
+% Load epanet input file
+d = epanet(settings.filename);
 
-%% Set default parameters
-output = default_parameters(input);
-settings.P = output.P;
-settings.B = output.B;
+% tmpInd = randperm(d.NodeCount);
+%Node Ingection
+nodeinj = 2; %tmpInd(1);
 
-%% Grid method
-output = gridmethod(output);
-settings.P = output.P;
+% initial settings quality are zero
+zeroNodes=zeros(1,d.NodeCount);
+d.setNodeInitialQuality(zeroNodes);
+% d.setLinkBulkReactionCoeff(zeros(1,d.LinkCount));
+% d.setLinkWallReactionCoeff(zeros(1,d.LinkCount));
 
-% Run Multiple Scenarios
-output = run_multiple_scenarios(settings);
+d.setNodeSourceType(nodeinj,'SETPOINT');
+pstep = d.TimePatternStep/3600;
+patlen=(d.TimeSimulationDuration/3600)/pstep;
 
-%% Plot graph
-% figure(1)
-% plot(results.Water_Age.Time/3600,results.Water_Age.Quality)
-% grid on
-% title('Water Age');
-% xlabel('Time (hours)')
-% ylabel('Hours')
-% legend(results.Network.NodeNameID)
+% add pattern for contamination at node injection
+tmppat=zeros(1,patlen);
+tmpstartstep=1; % hour
+tmpendstep=round(max(randperm(d.TimeSimulationDuration/3600)));
+tmppat(tmpstartstep:tmpendstep)=1;
+tmp1=d.addPattern('CONTAMINANT',tmppat);
+tmp2 = d.getNodePatternIndex;
+tmp2(nodeinj)=tmp1;
+tmpQ=zeros(1,d.NodeCount);
+tmpQ(nodeinj)=10;
+d.setNodeSourcePatternIndex(tmp2);
+d.setNodeSourceQuality(tmpQ)
+
+% solve hydraulics and quality analisis
+d.solveCompleteHydraulics;
+C=d.getComputedQualityTimeSeries('quality','time');
+
+% plots
+figure;
+plot(C.Time/3600,C.Quality);
+grid on
+title('Contaminant Concentration at Nodes');
+xlabel('Time (hours)')
+ylabel('Contaminant (mg/L)')
+legend(d.NodeNameID);
+
 %------------- END OF CODE --------------

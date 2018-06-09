ccf934d142144da72b485743359bedcc2045f26e
eldemet <eldemet@gmail.com>
2018-06-09 16:17:55 +0300

    moved function to get compiler name to matlab-tools folder

---
+++ matlab-tools/get_Compiler_Name.m
@@ -0,0 +1,31 @@
+function [ compilerName ] = get_Compiler_Name()
+%FUNCTION_NAME - One line description of what the function or script performs (H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%
+% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+%
+% Inputs:
+%    input1 - Description
+%
+% Outputs:
+%    output1 - Description
+%
+% Example: 
+%    Line 1 of example
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: none
+
+% Author        : Demetrios G. Eliades
+% Work address  : KIOS Research and Innovation Center of Excellence, University of Cyprus
+% email         : eldemet@ucy.ac.cy
+% Website       : http://www.kios.ucy.ac.cy
+% Last revision : 
+
+%------------- BEGIN CODE --------------
+    compilerName = mex.getCompilerConfigurations('C','Selected').Name();    
+end
+
